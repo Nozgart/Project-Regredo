10d2b53ffa44b0e58aa2e3835045c78f6a0cf26c
Nozgart <nozgart@gmail.com>
2020-12-08 21:15:32 +0300

    Попытка создать пустой проект с нормальным гитигнором

---
+++ Source/ProjectRegredo/ProjectRegredo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectRegredo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectRegredo, "ProjectRegredo" );
---
+++ Source/ProjectRegredo/ProjectRegredoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRegredoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREGREDO_API AProjectRegredoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
